b6fdf616d0cc63a5c7937528c16cd4b6e2c43a93
medsrk <rcpkarrinton@hotmail.co.uk>
2025-09-26 18:00:47 +0100

    first basic test
    - fixes accidental renamed go.mod
    - adds initial sanitiser test

--- internal/url/santiser_test.go
+++ internal/url/santiser_test.go
@@ -1,1 +1,22 @@
 package url
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestSanitiseURL_URLTooLong(t *testing.T) {
+	s := NewSanitiser()
+	s.MaxURLLength = 10
+
+	longURL := "http://example.com/this/is/a/very/long/url"
+	_, err := s.SanitiseURL(longURL)
+
+	if err == nil {
+		t.Error("Expected error for URL too long")
+	}
+
+	if err != nil && !strings.Contains(err.Error(), "URL length") {
+		t.Errorf("Unexpected error message: %v", err)
+	}
+}
